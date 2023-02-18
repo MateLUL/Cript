a2fbf255bb70a0c2b143451fef44e46064378331
Martin Nándor Csíkos <74834714+MateLUL@users.noreply.github.com>
2023-02-18 19:44:47 +0100

    Add files via upload

---
+++ Cript.java
@@ -0,0 +1,132 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.util.*;
+
+public class Cript {
+
+    public static String binaryEncode (String source, String input) {
+        //ArrayLists for storing the textToBeEncoded and converted encodedBinaries
+        ArrayList<String> textToBeEncoded = new ArrayList<>();
+        ArrayList<String> encodedBinaries = new ArrayList<>();
+
+
+        //Reading and storing the file's content in a variable
+        if ("file".equals(source)) {
+            try {
+                File inputFile = new File(input);
+                String fileContent = Files.readString(inputFile.toPath());
+                textToBeEncoded.add(fileContent);
+            }
+            catch (NoSuchFileException e) {
+                System.err.println("No such file detected.");
+                System.exit(1);
+            }
+            catch (IOException e) {
+                System.err.println("Cannot read file.");
+                System.exit(1);
+            }
+        }
+        else if ("text".equals(source)) {
+            textToBeEncoded.add(input);
+        }
+
+
+        //Converting the textToBeEncoded to binary
+        //Number of lines
+        for (int i = 0; i < textToBeEncoded.size(); i++) {
+            //Number of letters in a line
+            for (int j = 0; j < textToBeEncoded.get(i).length(); j++) {
+                int currentCharAscii = textToBeEncoded.get(i).charAt(j);
+                int[] binary = new int[8];
+
+                //Getting the binary form and storing in an array
+                int k = 0;
+                while (currentCharAscii > 0) {
+                    binary[k] = currentCharAscii % 2;
+                    currentCharAscii = currentCharAscii / 2;
+                    k++;
+                }
+
+                //Reversing the array
+                for (int l = 0; l < binary.length / 2; l++) {
+                    int temp = binary[l];
+                    binary[l] = binary[binary.length - l - 1];
+                    binary[binary.length - l - 1] = temp;
+                }
+
+                //Adding the converted binary to the encodedBinaries ArrayList
+                //it's disgusting, I know, needs refactoring
+                encodedBinaries.add(Arrays.toString(binary).replace(",", "").replace(" ", "").replace("[", "").replace("]", "").trim());
+            }
+        }
+
+
+        //Creating the output file, writing the encodedBinaries into it, and returning
+        if ("file".equals(source)) {
+            try {
+                File outputFile = new File(input.replace(".txt", "Encrypted.txt"));
+                FileWriter output = new FileWriter(outputFile);
+
+                for (int i = 0; i < encodedBinaries.size(); i++) {
+                    output.write(encodedBinaries.get(i) + " ");
+                }
+
+                output.close();
+                return "The encrypted text was saved in " + outputFile.getCanonicalPath();
+            }
+            catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        //If it was a text, clean up and return the encoded text
+        else {
+            String encodedText = encodedBinaries.toString().replace("[", "").replace("]", "").replace(",", "").trim();
+
+            return "The encrypted text: " + encodedText;
+        }
+    }
+
+    public static void errorHandling(String[] args) {
+        if (args.length != 1) {
+            if (args.length > 4) {
+                System.err.println("Too many arguments. Check the documentation, or type 'java Cript help' for further help.");
+                System.exit(1);
+            }
+            else if (args.length < 4) {
+                System.err.println("Not enough arguments. Check the documentation, or type 'java Cript help' for further help.");
+                System.exit(1);
+            }
+        }
+        else {
+            if ("help".equals(args[0])) {
+                //documentation
+                System.exit(0);
+            }
+            else {
+                System.err.println("Wrong type of argument. Check the documentation, or type 'java Cript help' for further help.");
+                System.exit(1);
+            }
+        }
+
+        if ((!"binary".equals(args[0])) || (!"encode".equals(args[1]) && !"decode".equals(args[1])) || (!"text".equals(args[2]) && !"file".equals(args[2]))) {
+            System.err.println("Wrong type of arguments. Check the documentation, or type 'java Cript help' for further help.");
+            System.exit(1);
+        }
+    }
+
+    public static void main(String[] args)  {
+        //Error handling
+        errorHandling(args);
+
+        //Storing the arguments in variables for readability
+        String method = args[0];
+        String process = args[1];
+        String source = args[2];
+        String input = args[3];
+
+        //Calling the appropriate functions
+        if ("binary".equals(method) && "encode".equals(process))
+            System.out.println(binaryEncode(source, input));
+    }
+}
