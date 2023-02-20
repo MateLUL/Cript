d2415654a40ef672ee9347ddb9f18afa0aaf2dc2
Martin Nándor Csíkos <74834714+MateLUL@users.noreply.github.com>
2023-02-20 21:21:08 +0100

    code refactored

---
+++ B64.java
@@ -0,0 +1,32 @@
+import java.util.Base64;
+
+public class B64 {
+    public static String encode () {
+        //Storing the text in a variable
+        String textToBeEncoded = TextIO.input();
+        String encodedText = Base64.getEncoder().encodeToString(textToBeEncoded.getBytes());
+
+
+        return TextIO.output(encodedText);
+    }
+
+    public static String decode () {
+        //Storing the text in a variable
+        String b64ToBeDecoded = TextIO.input();
+        String decodedText = "";
+
+
+        //Decoding the text and handling error
+        try {
+            byte[] decodedTextInBites = Base64.getDecoder().decode(b64ToBeDecoded);
+            decodedText = new String(decodedTextInBites);
+        }
+        catch (IllegalArgumentException e) {
+            System.err.println("Not a Base64 string (IllegalArgumentException).");
+            System.exit(1);
+        }
+
+
+        return TextIO.output(decodedText);
+    }
+}
